ed1a4ce21af35145c5d89524c8d67568aa86c15e
Dingadung <1229jimin@naver.com>
2023-03-18 15:22:58 +0900

    Add: Vitamin 도메인 추가

--- backend/vita-web/src/main/java/com/sejong/vitaweb/vo/vitamin.java
+++ backend/vita-web/src/main/java/com/sejong/vitaweb/vo/vitamin.java
@@ -16,6 +16,7 @@ public class vitamin implements Serializable {
   private String link; // 구매 링크
   private String img_path; // 이미지 링크
 
+  // 비타민 정보들
   private double e;
   private double d;
   private double a;
@@ -27,7 +28,17 @@ public class vitamin implements Serializable {
   private double b9;
   private double b12;
   private double c;
-  private double
+  private double cr;
+  private double mo;
+  private double i;
+  private double mn;
+  private double se;
+  private double cu;
+  private double zn;
+  private double fe;
+  private double mg;
+  private double ca;
+  private double lacto;
 
 
 }
