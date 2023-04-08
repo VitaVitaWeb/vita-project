e95ba9c3f5ee91510c79cb9f60f8bf1b95cafd66
Dingadung <1229jimin@sju.ac.kr>
2023-04-08 19:26:12 +0900

    Feat: search 제외 vita API 완성

--- backend/vita-web/src/main/java/com/sejong/vitaweb/service/DefaultVitaService.java
+++ backend/vita-web/src/main/java/com/sejong/vitaweb/service/DefaultVitaService.java
@@ -2,9 +2,7 @@ package com.sejong.vitaweb.service;
 
 import com.sejong.vitaweb.dao.NaverDao;
 import com.sejong.vitaweb.dao.VitaDao;
-import com.sejong.vitaweb.vo.NaverProductDto;
-import com.sejong.vitaweb.vo.Vitamin;
-import com.sejong.vitaweb.vo.VitaminDetail;
+import com.sejong.vitaweb.vo.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,8 +14,69 @@ import java.util.List;
 @Slf4j
 public class DefaultVitaService implements VitaService {
   private final VitaDao vitaDao;
-  private final NaverDao naverDao;
 
+  /**
+   * Vitamin Logic
+   */
+  @Override
+  public List<Vitamin> findVitalAll() throws Exception {
+    return vitaDao.findVitalAll();
+  }
+
+  @Override
+  public Vitamin findVitaByKey(int vno) throws Exception {
+    return vitaDao.findVitaByKey(vno);
+  }
+
+  @Override
+  public void updateVita(VitaminDetail vitaminDetail) throws Exception {
+    vitaDao.updateVitaDetail(vitaminDetail);
+  }
+
+
+  /**
+   * Vitamin Detail Logic
+   */
+  @Override
+  public VitaminDetail findVitalDetail(int vno) {
+    return vitaDao.findVitalDetail(vno);
+  }
+
+  @Override
+  public List<VitaminDetail> findVitalDetailAll() {
+    return vitaDao.findVitalDetailAll();
+  }
+
+
+  /**
+   * Vitamin Formulation Logic
+   */
+  @Override
+  public List<VitaFormulation> findVitaFormulationAll() {
+    return vitaDao.findVitaFormulationAll();
+  }
+  @Override
+  public VitaFormulation findVitaFormulationByKey(int vno){
+    return vitaDao.findVitaFormulationByKey(vno);
+  }
+
+
+  /**
+   * Vitamin Function Logic
+   */
+  @Override
+  public List<VitaFunction> findVitaFunctionAll() {
+    return vitaDao.findVitaFunctionAll();
+  }
+  @Override
+  public VitaFunction findVitaFunctionByKey(int vno) {
+    return vitaDao.findVitaFunctionByKey(vno);
+  }
+
+
+  /**
+   * Init Logic
+   */
   @Override
   public void insertVitaDB(List<NaverProductDto> prods){
     for(NaverProductDto prod : prods) {
@@ -53,31 +112,5 @@ public class DefaultVitaService implements VitaService {
       log.info("vno = {}", vitamin.getVno());
     }
   }
-
-  @Override
-  public List<Vitamin> findVitalAll() throws Exception {
-    return vitaDao.findVitalAll();
-  }
-
-  @Override
-  public Vitamin findVitaById(String id) throws Exception {
-    return vitaDao.findVitaById(id);
-  }
-
-  @Override
-  public void updateVita(VitaminDetail vitaminDetail) throws Exception {
-    vitaDao.updateVitaDetail(vitaminDetail);
-  }
-
-  @Override
-  public VitaminDetail findVitalDetail(int vno) {
-    return vitaDao.findVitalDetail(vno);
-  }
-
-  @Override
-  public List<VitaminDetail> findVitalDetailAll() {
-    return vitaDao.findVitalDetailAll();
-  }
-
 
 }
--- backend/vita-web/src/main/java/com/sejong/vitaweb/web/vita/VitaController.java
+++ backend/vita-web/src/main/java/com/sejong/vitaweb/web/vita/VitaController.java
@@ -1,9 +1,7 @@
 package com.sejong.vitaweb.web.vita;
 
 import com.sejong.vitaweb.service.VitaService;
-import com.sejong.vitaweb.vo.NaverProductDto;
-import com.sejong.vitaweb.vo.Vitamin;
-import com.sejong.vitaweb.vo.VitaminDetail;
+import com.sejong.vitaweb.vo.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +15,31 @@ public class VitaController {
     @Autowired
     VitaService vitaService;
 
+    /**
+     *
+     * Vitamin Logic
+     */
     @GetMapping("list")
     public List<Vitamin> findVitalAll() throws Exception {
         return vitaService.findVitalAll();
     }
 
-    // detail 관련
-    @GetMapping("detailList/{vno}")
+    @GetMapping("{vno}")
+    public Vitamin findVitamin(@PathVariable int vno) throws Exception {
+        log.info("vno = {}", vno);
+        return vitaService.findVitaByKey(vno);
+    }
+
+    @PutMapping("update")
+    public void updateVitaDetail(@RequestBody VitaminDetail vitaminDetail) throws Exception {
+        vitaService.updateVita(vitaminDetail);
+    }
+
+    /**
+     *
+     * Vitamin Detail Logic
+     */
+    @GetMapping("detail/{vno}")
     public VitaminDetail findVitaDetail(@PathVariable int vno) throws Exception {
         log.info("vno = {}", vno);
         return vitaService.findVitalDetail(vno);
@@ -34,11 +50,50 @@ public class VitaController {
         return vitaService.findVitalDetailAll();
     }
 
-    @PutMapping("update")
-    public void updateVitaDetail(@RequestBody VitaminDetail vitaminDetail) throws Exception {
-        vitaService.updateVita(vitaminDetail);
+    /**
+     *
+     * Vitamin Function Logic
+     */
+    @GetMapping("functionList")
+    public List<VitaFunction> functionList() {
+        return vitaService.findVitaFunctionAll();
+    }
+    @GetMapping("function/{vno}")
+    public VitaFunction findFunctionByKey(@PathVariable int vno) {
+        return vitaService.findVitaFunctionByKey(vno);
     }
 
+
+
+    /**
+     *
+     * Vitamin Formulation Logic
+     */
+    @GetMapping("formulationList")
+    public List<VitaFormulation> formulationList() {
+        return vitaService.findVitaFormulationAll();
+    }
+    @GetMapping("formulation/{vno}")
+    public VitaFormulation findFormulationByKey(@PathVariable int vno) {
+        return vitaService.findVitaFormulationByKey(vno);
+    }
+
+
+
+
+
+
+    /**
+     *
+     * Vitamin Search Logic
+     */
+
+
+
+
+    /**
+     * DB Init Logic
+     */
     @PostMapping("init-vita-function")
     public void initVitaFunction() throws Exception {
         vitaService.setVitaFunction();
