8329f6431938d4ec79870b06992a543c0df2b718
Lee-KyuHwun <107871734+Lee-Kyuhwun@users.noreply.github.com>
2023-04-29 04:25:23 +0900

    Add files via upload
    영양제 하루 권장량 테이블 생성 및 데이터 주입 코드 sql 문

---
+++ database/영양제 하루 권장량 테이블 생성 및 데이터 주입 코드.sql
@@ -0,0 +1,34 @@
+CREATE TABLE vitadb.vita_recommend (
+e FLOAT,
+d FLOAT,
+a FLOAT,
+b1 FLOAT,
+b2 FLOAT,
+b3 FLOAT,
+b5 FLOAT,
+b9 FLOAT,
+b12 FLOAT,
+b7 FLOAT,
+c FLOAT,
+cr FLOAT,
+mo FLOAT,
+i FLOAT,
+mn FLOAT,
+se FLOAT,
+cu FLOAT,
+zn FLOAT,
+fe FLOAT,
+mg FLOAT,
+ca FLOAT,
+lacto FLOAT,
+lutein FLOAT,
+milk_thistle FLOAT,
+omega3 FLOAT,
+msm FLOAT,
+propolis FLOAT,
+collagen FLOAT,
+theanine FLOAT
+);
+
+INSERT INTO vitadb.vita_recommend (e,d,a,b1,b2,b3,b5,b9,b12,b7,c,cr,mo,i,mn,se,cu,zn,fe,mg,ca,lacto,lutein,milk_thistle,omega3,msm,propolis,collagen,theanine) VALUES
+	 (12.0,10.0,800.0,100.0,15.0,1.7,20.0,5.0,400.0,2.4,30.0,100.0,25.0,45.0,150.0,310.0,55.0,900.0,11.0,10.0,2.0,1000.0,10.0,130.0,250.0,1000.0,500.0,1000.0,200.0);
