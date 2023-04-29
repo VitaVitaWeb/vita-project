41b6fd814db24521d2d98e9a4714172030d42025
이규훈 <wxyz0904@naver.com>
2023-04-29 14:47:32 +0900

    찜하기 구현

--- backend/vita-web/src/main/java/com/sejong/vitaweb/dao/VitaLikeDao.java
+++ backend/vita-web/src/main/java/com/sejong/vitaweb/dao/VitaLikeDao.java
@@ -1,2 +1,12 @@
-package com.sejong.vitaweb.dao;public interface VitaLikeDao {
+package com.sejong.vitaweb.dao;
+
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface VitaLikeDao {
+    @Insert("INSERT INTO vitadb.vita_like (status, id, vno) VALUES (#{status}, #{id}, #{vno})")
+    void insertVitaLike(@Param("status") String status, @Param("id") String id, @Param("vno") int vno);
 }
--- backend/vita-web/src/main/java/com/sejong/vitaweb/service/VitaLikeService.java
+++ backend/vita-web/src/main/java/com/sejong/vitaweb/service/VitaLikeService.java
@@ -1,2 +1,18 @@
-package com.sejong.vitaweb.service;public class VitaLikeService {
+package com.sejong.vitaweb.service;
+
+import com.sejong.vitaweb.dao.VitaLikeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VitaLikeService {
+    private final VitaLikeDao vitaLikeDao;
+
+    public VitaLikeService(VitaLikeDao vitaLikeDao) {
+        this.vitaLikeDao = vitaLikeDao;
+    }
+
+    public void addVitaLike(String status, String id, int vno) {
+        vitaLikeDao.insertVitaLike(status, id, vno);
+    }
 }
