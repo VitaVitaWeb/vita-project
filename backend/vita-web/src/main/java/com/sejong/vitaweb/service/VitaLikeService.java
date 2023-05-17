2d1014b88b55ca5e1702795ce15785a8076ff479
이규훈 <wxyz0904@naver.com>
2023-05-18 00:51:54 +0900

    영양제 별 찜하기 갯수 구현

--- backend/vita-web/src/main/java/com/sejong/vitaweb/service/VitaLikeService.java
+++ backend/vita-web/src/main/java/com/sejong/vitaweb/service/VitaLikeService.java
@@ -1,16 +1,18 @@
 package com.sejong.vitaweb.service;
 
 import com.sejong.vitaweb.dao.VitaLikeDao;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+
+
+// 사용자별 찜하기
+@RequiredArgsConstructor
 @Service
 public class VitaLikeService {
     private final VitaLikeDao vitaLikeDao;
-
-    public VitaLikeService(VitaLikeDao vitaLikeDao) {
-        this.vitaLikeDao = vitaLikeDao;
-    }
 
     public void addVitaLike(String status, String id, int vno) {
         vitaLikeDao.insertVitaLike(status, id, vno);
