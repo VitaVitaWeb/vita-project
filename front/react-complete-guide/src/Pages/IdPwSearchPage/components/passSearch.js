cf2833db9b9339f9aa7df1af1688f75fa6535549
이규훈 <wxyz0904@naver.com>
2023-05-30 14:25:20 +0900

    비밀번호 이메일 성공 초기화는실패

--- front/react-complete-guide/src/Pages/IdPwSearchPage/components/passSearch.js
+++ front/react-complete-guide/src/Pages/IdPwSearchPage/components/passSearch.js
@@ -1,6 +1,12 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./searchPage.css";
 import TopBar from "../../../CommonComponent/TopBar/topBar";
+
 function PassSearch(props) {
+  const [email, setEmail] = useState("");
+  const [code, setCode] = useState("");
+
   const OnclickIdButton = () => {
     props.setOnIdSearch(true);
   };
@@ -8,6 +14,34 @@ function PassSearch(props) {
   const OnclickPwButton = () => {
     props.setOnIdSearch(false);
   };
+
+  const requestVerifyCode = async () => {
+    try {
+      await axios.post("/member/findpwd", { email: email });
+      alert("인증 코드를 발급하였습니다. 이메일을 확인해주세요.");
+    } catch (err) {
+      alert("인증 코드 발급에 실패하였습니다.");
+    }
+  };
+
+  const verifyCode = async () => {
+    try {
+      let response = await axios.post("/member/verifypwd", {
+        email: email,
+        code: code,
+      });
+      if (response.data) {
+        alert("인증 코드가 확인되었습니다. 비밀번호를 재설정하세요.");
+        // 코드가 맞다면 비밀번호를 재설정할 수 있게 합니다.
+        /* 비밀번호 재설정 로직 */
+      } else {
+        alert("인증 코드가 잘못되었습니다. 다시 시도해주세요.");
+      }
+    } catch (err) {
+      alert("인증 코드 확인에 실패하였습니다.");
+    }
+  };
+
   return (
     <div>
       <TopBar></TopBar>
@@ -29,15 +63,26 @@ function PassSearch(props) {
               <input
                 className="idSearchPageInput"
                 placeholder="이메일:"
-              ></input>
-              <button className="searchPageCodeButton">인증코드 보내기</button>
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <button
+                className="searchPageCodeButton"
+                onClick={requestVerifyCode}
+              >
+                인증코드 보내기
+              </button>
             </div>
             <div>
               <input
                 className="idSearchPageInput"
                 placeholder="인증코드:"
-              ></input>
-              <button className="searchPageCodeButton">인증코드 확인</button>
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+              <button className="searchPageCodeButton" onClick={verifyCode}>
+                인증코드 확인
+              </button>
             </div>
           </div>
         </div>
