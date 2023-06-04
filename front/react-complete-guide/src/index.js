aa4639267244ad193dd30c565d790b3ebfb37ffd
준원 <swjwpower@gmail.com>
2023-06-04 22:04:28 +0900

    store 복구

--- front/react-complete-guide/src/index.js
+++ front/react-complete-guide/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import store from "./store/index";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CustomerInfoProvider } from "./customerInfo"; // Add this line
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CustomerInfoProvider>
-    <App />
-  </CustomerInfoProvider>
+  <Provider store={store}>
+    <CustomerInfoProvider>
+      <App />
+    </CustomerInfoProvider>
+  </Provider>
 );
